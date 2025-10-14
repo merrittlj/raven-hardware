31a441e2f5b5e52f66cb6119796f4e3c3373e9db
Lucas Merritt <merrittlj@protonmail.com>
2025-10-14 15:23:46 -0600

    full build123d migration + holder additions

--- case/build.py
+++ case/build.py
@@ -1,5 +1,7 @@
 import timeit
+import math
 from build123d import *
+from build123d import extrude as _extrude
 
 start_time = timeit.default_timer()
 
@@ -41,15 +43,6 @@ power_width = 2.6
 power_height = 1.6
 power_center_offset = -3.663
 
-# for the strap "holder"
-# ideally for ~20-21mm nato strap
-holder_width = width - 4 
-holder_base = 10 # 6
-holder_height = 5
-holder_inside_base = holder_base - 3
-holder_inside_width = holder_width - 6 
-holder_inside_outside = 2
-
 # thickness: battery + usb + pcb + epd + bezel
 battery_thickness = 3
 pcb_thickness = 1.6
@@ -57,15 +50,37 @@ epd_thickness = 1.05
 # eg. total wall height
 thickness = battery_thickness + usb_height + pcb_thickness + epd_thickness + bezel_thickness
 
+# for the nato strap "holder"
+# ideally ~20mm lug width
+# 26 mm strap length(?)
+lug_width = 18
+
+holder_width = width - 8 
+holder_height = thickness * 0.4
+holder_inside_width = lug_width + 0.2 
+holder_inside_outside = 2
+holder_inside_base = 3
+holder_base = holder_inside_base + holder_inside_outside
+holder_drop = 3
+
+assert holder_inside_width < holder_width
+assert holder_width < width
+assert holder_inside_base < holder_base
+
 component_level = bezel_thickness + epd_thickness + pcb_thickness
 
 # from outside of bezel to case edge
 wall_thickness = 1.2
 
-def wall_hole(wall, side, hole_width, hole_height, center_offset, level):
+print(f"lug to lug: {height + wall_thickness * 2 + holder_base * 2}")
+print(f"thickness: {thickness}")
+print(f"width: {width + wall_thickness * 2}")
+print(f"height: {height + wall_thickness * 2}")
+
+def wall_hole(walls, side, hole_width, hole_height, center_offset, level):
     f = {
-        "left": lambda: wall.faces().sort_by(Axis.X)[0],
-        "right": lambda: wall.faces().sort_by(Axis.X)[-1],
+        "left": lambda: walls.faces().sort_by(Axis.X)[0],
+        "right": lambda: walls.faces().sort_by(Axis.X)[-1],
     }[side]()
     be = f.edges().sort_by(Axis.Z)[0]
     p = Plane(be @ 0.5, x_dir=be.tangent_at(0.5), z_dir=f.normal_at())
@@ -73,6 +88,40 @@ def wall_hole(wall, side, hole_width, hole_height, center_offset, level):
     y_offset = -level + hole_height_padding if side == "left" else level - hole_height_padding
     hole = Rectangle(hole_width + hole_width_padding * 2, hole_height + hole_height_padding * 2, align=hole_align)
     return extrude(p * Pos(center_offset, y_offset, 0) * hole, -wall_thickness)
+
+def extrude(shape, dir, center=False):
+    if center:
+        return _extrude(shape, dir/2) + _extrude(shape, -dir/2)
+    else:
+        return _extrude(shape, dir)
+
+def holder(walls, side):
+    f = {
+        "top": lambda: walls.faces().filter_by(Axis.Y).sort_by(Axis.Y)[-1],
+        "bottom": lambda: walls.faces().filter_by(Axis.Y).sort_by(Axis.Y)[0],
+    }[side]()
+    e = f.edges().filter_by(GeomType.LINE).sort_by(Axis.Y)[-1]
+    
+    x_dir = f.normal_at()
+    desired_y = Vector(0, 0, -1)
+    crossed_z = x_dir.cross(desired_y)
+    
+    holder_plane = Plane(e @ 0.5, x_dir=x_dir, z_dir=crossed_z) * Pos(Y=-holder_drop)
+    holder_plane_x = Axis(origin=(0, 0, 0), direction=holder_plane.x_dir)
+    holder_shape = holder_plane * Polygon(*[(0, 0), (holder_base, -holder_drop), (0, holder_height)], align=(Align.MIN, Align.MIN))
+    tip = holder_shape.vertices().sort_by(holder_plane_x)[-1]
+    holder_shape = fillet(tip, radius=.25)
+    holder = extrude(holder_shape, holder_width, center=True)
+
+    holder_shape_flat = holder_plane * Polygon(*[(0, 0), (holder_base, 0), (0, holder_height)], align=(Align.MIN, Align.MIN))
+    holder_flat = extrude(holder_shape_flat, holder_width, center=True)
+
+    ff = holder_flat.faces().filter_by(Axis.Z)[-1]
+    fe = ff.edges().sort_by(holder_plane_x)[-1]
+    holder_inside_shape = Plane(fe @ 0.5, x_dir=Plane(ff).x_dir, z_dir=Plane(ff).z_dir) * Pos(X=-holder_inside_outside) * Rectangle(holder_inside_base, holder_inside_width, align=(Align.MAX, Align.CENTER))
+    holder -= extrude(holder_inside_shape, -holder_height*2)
+
+    return holder
 
 base = Rectangle(width, height)
 bezel_inner = Rectangle(
@@ -86,44 +135,20 @@ walls_shape = offset(bezel_shape, wall_thickness) - bezel_shape
 bezel_shape -= fillet((Location(tope @ 0.5) * Pos(Y=-bezel_top) * bezel_inner).vertices(), 2)
 bezel = extrude(bezel_shape, bezel_thickness)
 
-wall = extrude(walls_shape, thickness)
+walls = extrude(walls_shape, thickness)
 
-wall -= wall_hole(wall, "left", usb_width, usb_height, usb_center_offset, component_level)
+walls -= wall_hole(walls, "left", usb_width, usb_height, usb_center_offset, component_level)
 
-wall -= wall_hole(wall, "left", button_width, button_height, button_center_offset, component_level)
-wall -= wall_hole(wall, "right", button_width, button_height, button_center_offset, component_level)
-wall -= wall_hole(wall, "left", button_width, button_height, -button_center_offset, component_level)
-wall -= wall_hole(wall, "right", button_width, button_height, -button_center_offset, component_level)
+walls -= wall_hole(walls, "left", button_width, button_height, button_center_offset, component_level)
+walls -= wall_hole(walls, "right", button_width, button_height, button_center_offset, component_level)
+walls -= wall_hole(walls, "left", button_width, button_height, -button_center_offset, component_level)
+walls -= wall_hole(walls, "right", button_width, button_height, -button_center_offset, component_level)
 
-wall -= wall_hole(wall, "right", power_width, power_height, power_center_offset, component_level)
+walls -= wall_hole(walls, "right", power_width, power_height, power_center_offset, component_level)
 
-topf = wall.faces().filter_by(Axis.Y).sort_by(Axis.Y)[-1]
-tope = topf.edges().filter_by(GeomType.LINE).sort_by(Axis.Y)[-1]
+top_holder = holder(walls, "top")
+bottom_holder = holder(walls, "bottom")
+holders = Compound(top_holder + bottom_holder)
 
-# holder_shape = Plane(tope @ 0.5, x_dir=(0, 1, 0), y_dir=(0, 0, 1)) * Polygon((0, 0), (holder_base, 0), (0, holder_height))
-# holder_shape = Plane.YZ * Rot(0, -180, 0) * Polygon((0, 0), (holder_base, 0), (0, holder_height))
-holder_shape = Plane.XY * Rot(90, -90, 180) * Polygon((0, 0), (holder_base, 0), (0, holder_height))
-holder = extrude(holder_shape, holder_width)
-holder_shape1 = Plane.XY * Rot(90, -90+180, 180) * Polygon((0, 0), (holder_base, 0), (0, holder_height))
-holder1 = extrude(holder_shape1, holder_width)
-# bf = holder.faces().filter_by(Axis.Y)[0]
-# holder_inside_shape = Plane(bf) * Pos(X=-holder_inside_outside) * Rectangle(holder_inside_base, holder_inside_width, align=(Align.MIN, Align.CENTER))
-# holder -= extrude(holder_inside_shape, -holder_height)
-
-# top_holder_shape = right_triangle([holder_base, holder_height], center=True)
-# top_holder_inside_shape = right_triangle([holder_inside_base, holder_inside_height], center=True).translateY(holder_inside_extra)
-# top_holder = top_holder_shape.linear_extrude(holder_width, center=True)
-# top_holder = top_holder - top_holder_inside_shape.linear_extrude(holder_inside_width, center=True)
-# top_holder = top_holder.rotate([-90, 0, 90]).translate([0, height / 2 + wall_thickness * 2, thickness - holder_height / 2])
-
-# bottom_holder_shape = right_triangle([holder_base, holder_height], center=True)
-# bottom_holder_inside_shape = right_triangle([holder_inside_base, holder_inside_height], center=True).translateY(holder_inside_extra)
-# bottom_holder = bottom_holder_shape.linear_extrude(holder_width, center=True)
-# bottom_holder = bottom_holder - bottom_holder_inside_shape.linear_extrude(holder_inside_width, center=True)
-# bottom_holder = bottom_holder.rotate([-90, 0, -90]).translate([0, -height / 2 - wall_thickness * 2 - bottom_side_padding, thickness - holder_height / 2])
-
-# model = bezel + wall + holder
-model = bezel + holder + holder1
+model = bezel + walls + holders
 export_stl(model, "build.stl")
-
-print(f"Time: {timeit.default_timer() - start_time:0.3f}s")
